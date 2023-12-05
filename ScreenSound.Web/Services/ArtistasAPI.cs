05ab1f89b1d5fb4f61610d08a3ffa36f5a872e61
bessax <andrebessax@gmail.com>
2023-12-05 15:07:06 -0300

    #Vídeo 1.4 - Preparação do cenário para o consumo da API.

---
+++ ScreenSound.Web/Services/ArtistasAPI.cs
@@ -0,0 +1,19 @@
+﻿using ScreenSound.Web.Response;
+using System.Net.Http.Json;
+using System.Reflection.Metadata;
+
+namespace ScreenSound.Web.Services;
+
+public class ArtistasAPI
+{
+    private readonly HttpClient _httpClient;
+    public ArtistasAPI(IHttpClientFactory factory)
+    {
+        _httpClient = factory.CreateClient("API");
+    }
+
+    public async Task<ICollection<ArtistaResponse>?> GetArtistasAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<ICollection<ArtistaResponse>>("artistas");
+    }
+}
