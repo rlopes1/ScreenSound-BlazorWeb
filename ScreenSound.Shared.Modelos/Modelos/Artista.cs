2e1f66c6a75a5ebbada2087b6c57e8f731abca75
bessax <andrebessax@gmail.com>
2023-12-08 02:33:16 -0300

    #Vídeo 3.3 - Exibindo a imagem cadastrada no Card.

--- ScreenSound.Shared.Modelos/Modelos/Artista.cs
+++ ScreenSound.Shared.Modelos/Modelos/Artista.cs
@@ -5,13 +5,12 @@ public class Artista
     public virtual ICollection<Musica> Musicas { get; set; } = new List<Musica>();
     public Artista()
     {
-       
+        
     }
     public Artista(string nome, string bio)
     {
         Nome = nome;
-        Bio = bio;
-       
+        Bio = bio;        
     }
 
     public string Nome { get; set; }
