7ba22f7d8de23898b56909a05d2a5285ac4ef634
bessax <andrebessax@gmail.com>
2023-12-08 16:39:32 -0300

    #

--- ScreenSound.Web/Response/GeneroResponse.cs
+++ ScreenSound.Web/Response/GeneroResponse.cs
@@ -1,2 +1,7 @@
 ﻿namespace ScreenSound.Web.Response;
-public record GeneroResponse(int Id, string Nome, string Descricao);
+public record GeneroResponse(int Id, string Nome, string Descricao) {
+    public override string ToString()
+    {
+        return $"{this.Nome}";
+    }
+}
