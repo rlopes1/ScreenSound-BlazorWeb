0e39df1c2ea3dc6987a8f9d3812c373334084c93
rlopes1 <rtaboada@gmail.com>
2025-06-15 11:17:12 -0300

    Aula 5

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -15,7 +15,9 @@ public class ScreenSoundContext: DbContext
     public DbSet<Musica> Musicas { get; set; }
     public DbSet<Genero> Generos { get; set; }
 
-    private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+    private string connectionString = "Server=tcp:screensoundserver2.database.windows.net,1433;Initial Catalog=screensoundV0;Persist Security Info=False;User ID=taboada;Password=Malaka312@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+    //"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"
 
     public ScreenSoundContext(DbContextOptions options) : base(options)
     {
@@ -42,6 +44,14 @@ public class ScreenSoundContext: DbContext
         modelBuilder.Entity<Musica>()
             .HasMany(c => c.Generos)
             .WithMany(c => c.Musicas);
+
+        // Relacionamento: Um Artista tem muitas Músicas, uma Música tem um Artista
+        modelBuilder.Entity<Musica>()
+            .HasOne(m => m.Artista)
+            .WithMany(a => a.Musicas)
+            .HasForeignKey(m => m.ArtistaId)
+            .OnDelete(DeleteBehavior.SetNull); // ou .OnDelete(DeleteBehavior.Restrict) conforme sua regra de negócio
     }
 
+    
 }
